3c8c46ff23ae0d1366632c6bb1bb5fe7dde39f57
JoaquinMun <joaqu.munozs@duocuc.cl>
2025-05-26 00:10:09 -0400

    Se agrego Repository de Seller, se agrego la logica del crud y se llamo las funciones a controller.

--- Perfulandia/Perfulandia/src/main/java/cl/duocuc/rodrcruz/Perfulandia/controller/SellerController.java
+++ Perfulandia/Perfulandia/src/main/java/cl/duocuc/rodrcruz/Perfulandia/controller/SellerController.java
@@ -1,5 +1,49 @@
 package cl.duocuc.rodrcruz.Perfulandia.controller;
 
+import cl.duocuc.rodrcruz.Perfulandia.model.Seller;
+import cl.duocuc.rodrcruz.Perfulandia.service.SellerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Sellers")
 public class SellerController {
+    @Autowired
+    private SellerService sellerService;
+
+    @GetMapping("/{ElementId}")
+    public ResponseEntity<Seller> getSellers(@PathVariable int ElementId) {
+        Seller found = sellerService.getPotitionById(ElementId);
+        if (found == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(found);
+    }
+    @PutMapping("/{ElementId}")
+    public ResponseEntity<Seller> PutSellers(@PathVariable int ElementId, @RequestBody Seller request) {
+        Seller found = sellerService.getPotitionById(ElementId);
+        if (found == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(found);
+    }
+    @PostMapping
+    public ResponseEntity<Seller> PostSellers(@RequestBody Seller request) {
+        Seller found = sellerService.addSeller(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(found);
+    }
+    @DeleteMapping("/{ElementId}")
+    public ResponseEntity<Seller> deleteSeller(@PathVariable int ElementId){
+        Seller delete = sellerService.getPotitionById(ElementId);
+        if (delete== null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(delete);
+    }
+
 
 }
