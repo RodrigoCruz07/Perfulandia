d4aa8d98132585aa87367b225e17f3f69aee7c60
miguelcamposdasilva <migu.campos@duocuc.cl>
2025-07-03 15:07:42 -0400

    Se añade PurchaControllerTest

--- Perfulandia/Perfulandia/perfume-inventory-purchase/src/main/java/cl/duoc/rodrcruz/perfumeinventorypurchase/controller/request/PurchaseRequest.java
+++ Perfulandia/Perfulandia/perfume-inventory-purchase/src/main/java/cl/duoc/rodrcruz/perfumeinventorypurchase/controller/request/PurchaseRequest.java
@@ -1,8 +1,10 @@
 package cl.duoc.rodrcruz.perfumeinventorypurchase.controller.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
